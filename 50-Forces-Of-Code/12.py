2c1add2d03245d84e1d61cab4eede9ffd526947a
HumbleSprinter <43790128+HumbleSprinter@users.noreply.github.com>
2018-10-02 22:39:20 +0530

    added documentation

--- 50-Forces-Of-Code/12.py
+++ 50-Forces-Of-Code/12.py
@@ -2,7 +2,7 @@ n,k=map(int,input().split())
 a=list(map(int,input().split()))
 p=0
 for i in range(len(a)):
-    if(a[i]%k==0 and a[i]//k > p):
+    if(a[i]%k==0 and a[i]//k > p): # // is used for integer divison
         p=a[i]//k
         
 print(p)        
