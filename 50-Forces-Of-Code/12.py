55693727baa1fd5096465919957035aa14925ac5
HumbleSprinter <43790128+HumbleSprinter@users.noreply.github.com>
2019-10-08 14:26:37 +0530

    Update 12.py

--- 50-Forces-Of-Code/12.py
+++ 50-Forces-Of-Code/12.py
@@ -3,6 +3,8 @@ a=list(map(int,input().split()))
 p=0
 for i in range(len(a)):
     if(a[i]%k==0 and a[i]//k > p):
-        p=a[i]//k
+        p=a[i]//kint
+    else:
+        print()
         
 print(p)        
